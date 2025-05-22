a06f4fe8f4adab521190db546267b60fe6f8dfc4
Marion Vives <mvives.dev@gmail.com>
2025-05-22 12:16:55 +0200

    P2C3 creation

---
+++ exercices/P2C3.py
@@ -0,0 +1,21 @@
+# Créez une fonction salaire_mensuel(salaire_annuel), en paramètre un salaire annuel,
+# retourne le salaire mensuel correspondant en divisant le salaire annuel par 12.
+
+# Créez une fonction salaire_hebdomadaire(salaire_mensuel), en paramètre un salaire mensuel
+# retourne le salaire hebdomadaire correspondant en divisant le salaire mensuel par 4.
+
+# Créez une fonction salaire_horaire(salaire_hebdomadaire, heures_travaillees), en paramètres un salaire hebdomadaire
+# et le nombre d'heures travaillées par semaine,retourne le salaire horaire correspondant en divisant le salaire hebdomadaire
+# par le nombre d'heures travaillées par semaine.
+
+# Demandez à l'utilisateur de saisir son salaire annuel.
+
+# Demandez à l'utilisateur de saisir le nombre d'heures travaillées par semaine.
+
+# Appelez les fonctions précédemment créées pour calculer le salaire horaire correspondant.
+
+# Affichez le résultat sous la forme : "Votre salaire horaire est de XX euros".
+
+# Attention : n'oubliez pas de convertir les valeurs fournies (chaînes de caractères) en nombres.
+# Il est possible que les valeurs fournies soient des valeurs à virgule.
+# Il faut alors utiliser float plutôt que int pour faire la conversion !
