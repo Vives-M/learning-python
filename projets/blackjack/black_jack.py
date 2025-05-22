4d0f11b0c996a0f9e6cdbfee0ae6e66c9db0e416
Marion Vives <mvives.dev@gmail.com>
2025-05-22 15:43:07 +0200

    implement black_jack.py

--- projets/blackjack/black_jack.py
+++ projets/blackjack/black_jack.py
@@ -1,3 +1,12 @@
-# Implement the pick_bank_score method which returns a random bank score between 16 and 21.
+# Implémente la fonction score_pioche_banque qui retourne un nombre aléatoire entre 16 et 21
+import random
+def score_pioche_banque():
+    liste_scores_possibles_banque = range(1,22)
+    return random.choice(liste_scores_possibles_banque)
+print("Score banque : ", score_pioche_banque())
 
-# Implement #pick_player_card which returns a random card value between 1 and 11.
+# Implémente la fonction pioche_carte_joueur qui retourne un nombre aléatoire entre 1 et 11
+def pioche_carte_joueur():
+    liste_scores_possibles_joueur = range(1,12)
+    return random.choice(liste_scores_possibles_joueur)
+print("Score joueur : ", pioche_carte_joueur())
