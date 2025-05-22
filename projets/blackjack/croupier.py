790ccb026f12392a179e517125714b18fe9da0b2
Marion Vives <mvives.dev@gmail.com>
2025-05-22 16:09:45 +0200

    implement croupier.py

--- projets/blackjack/croupier.py
+++ projets/blackjack/croupier.py
@@ -1,3 +1,17 @@
-# Implement the state_of_the_game method which builds a message containing the bank and player’s scores.
+# Implémente la fonction etat_du_jeu qui crée le message indiquant les scores de la banque et du joueur
+def etat_de_jeu(score_banque, score_joueur):
+    return print(f"Votre score est de {score_joueur}, la banque a {score_banque}.")
 
-# Implement the end_game_message method to be called at the end of the game, containing the game outcome (win, lose or push)
+
+# Implémente la fonction message_fin_du_jeu qui sera appelée à la fin du jeu, qui contient le résultat (victoire, défaite ou push)
+def message_fin_de_jeu(score_banque, score_joueur):
+    if score_joueur > 21:
+        return print("Vous avez dépassé 21, LOSER !")
+    elif score_joueur == 21:
+        return print("Félicitations ! VICTOIRE par BLACKJACK !")
+    elif score_joueur > score_banque:
+        return print("Félicitations, vous avez plus de points que la banque, WINNER !")
+    elif score_joueur < score_banque:
+        return print("Vous avez moins de points que la banque, LOSER !")
+    else:
+        return print("C'est un push ! Vous pouvez repartir avec votre argent.")
