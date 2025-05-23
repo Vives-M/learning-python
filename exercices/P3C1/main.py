ef361e2b2fd19093466ccb8657be0c42df836f78
Marion Vives <mvives.dev@gmail.com>
2025-05-23 11:42:46 +0200

    P3C1

---
+++ exercices/P3C1/main.py
@@ -0,0 +1,10 @@
+# importez le module operations.py
+import operations
+
+# addition de 3 et 5 ;
+resulat_addition = operations.addition(3,5)
+print(resulat_addition)
+
+# multiplication de 8 et 2
+resultat_multiplication = operations.multiplication(8,2)
+print(resultat_multiplication)
---
+++ exercices/P3C1/operations.py
@@ -0,0 +1,7 @@
+# addition(a, b): qui retourne la somme de deux nombres a et b
+def addition(a, b):
+    return a + b
+
+# multiplication(a, b): qui retourne le produit de deux nombres a et b
+def multiplication(a, b):
+    return a*b
