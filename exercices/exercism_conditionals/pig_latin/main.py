42bf61b529685513ee15eacdf4eed28ba9c006dc
Marion Vives <mvives.dev@gmail.com>
2025-06-02 16:53:08 +0200

    modification du and l28 + ajout sécurité si mot inférieur ou égal à 2 lettres

--- exercices/exercism_conditionals/pig_latin/main.py
+++ exercices/exercism_conditionals/pig_latin/main.py
@@ -20,48 +20,51 @@ def translate_word(word):
     vowels = ["a", "e", "i", "o", "u"]
     consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x" ,"y" ,"z"]
     pig_latin_adding = "ay"
-# si un mot commence avec une voyelle ou avec "xr" ou "yt", ajoute "ay" à la fin du mot
-    if word[0] in vowels or word[0:2] == "xr" or word[0:2] == "yt":
-        return word + pig_latin_adding
-# si un mot commence avec une ou plusieurs consonnes suivies de "y", déplace les consonnes précédant le "y" à la fin du mot et ajoute "ay" à la fin
-    if word[0] in consonants and word[1] == "y":
-        new_word = (word + word[0])[1:]
-        print(new_word)
-        return new_word + pig_latin_adding
-    if word[0] and word[1] in consonants and word[2] == "y":
-        new_word = (word + word[:2])[2:]
-        print(new_word)
-        return new_word + pig_latin_adding
-# si un mot commence avec aucune ou plusieurs consonnes suivies de "qu", déplace ces consonnes à la fin du mot et ajoute "ay" à la fin
-    # if "qu" == word[:2]:
-    #     new_word = (word + word[:2])[2:]
-    #     print(new_word)
-    #     return new_word + pig_latin_adding
-    if word.startswith("qu"):
-        new_word = (word + word[:2])[2:]
-        print(new_word)
-        return new_word + pig_latin_adding
-    # if word[1] == "q" and word[2] == "u" :
-    #     new_word = (word + word[:3])[3:]
-    #     print(new_word)
-    #     return new_word + pig_latin_adding
-    if word.startswith("qu",1):
-        new_word = (word + word[:3])[3:]
-        print(new_word)
-        return new_word + pig_latin_adding
-# si un mot commence avec une ou plusieurs consonnes, déplace ces consonnes à la fin du mot et ajoute "ay" à la fin
-    if word[0] in consonants and word[1] in consonants and word[2] in consonants :
-        new_word = (word + word[0] + word[1] + word[2])[3:]
-        print(new_word)
-        return new_word + pig_latin_adding
-    if word[0] in consonants and word[1] in consonants :
-        new_word = (word + word[:2])[2:]
-        print(new_word)
-        return new_word + pig_latin_adding
-    if word[0] in consonants:
-        new_word = (word + word[0])[1:]
-        print(new_word)
-        return new_word + pig_latin_adding
+    if len(word) > 2:
+        print(len(word))
+    # si un mot commence avec une voyelle ou avec "xr" ou "yt", ajoute "ay" à la fin du mot
+        if word[0] in vowels or word[0:2] == "xr" or word[0:2] == "yt":
+            return word + pig_latin_adding
+    # si un mot commence avec une ou plusieurs consonnes suivies de "y", déplace les consonnes précédant le "y" à la fin du mot et ajoute "ay" à la fin
+
+        if word[0] in consonants and word[1] == "y":
+            new_word = (word + word[0])[1:]
+            print(new_word + pig_latin_adding)
+            return new_word + pig_latin_adding
+        if word[0] in consonants and word[1] in consonants and word[2] == "y":
+            new_word = (word + word[:2])[2:]
+            print(new_word + pig_latin_adding)
+            return new_word + pig_latin_adding
+
+    # si un mot commence avec aucune ou plusieurs consonnes suivies de "qu", déplace ces consonnes à la fin du mot et ajoute "ay" à la fin
+    # It would be simpler and safer to use a loop that goes and looks for the first vowel, wherever that may be, and then split the word there.
+    # (The same holds for how you assume that a vowel 'y' will always be the second or third letter in the word.
+    # Sometimes it's the fourth, as in spry or strychnine.)
+        if word.startswith("qu"):
+            new_word = (word + word[:2])[2:]
+            print(new_word + pig_latin_adding)
+            return new_word + pig_latin_adding
+        if word.startswith("qu",1):
+            new_word = (word + word[:3])[3:]
+            print(new_word + pig_latin_adding)
+            return new_word + pig_latin_adding
+    # si un mot commence avec une ou plusieurs consonnes, déplace ces consonnes à la fin du mot et ajoute "ay" à la fin
+        if word[0] in consonants and word[1] in consonants and word[2] in consonants :
+            new_word = (word + word[0] + word[1] + word[2])[3:]
+            print(new_word + pig_latin_adding)
+            return new_word + pig_latin_adding
+        if word[0] in consonants and word[1] in consonants :
+            new_word = (word + word[:2])[2:]
+            print(new_word + pig_latin_adding)
+            return new_word + pig_latin_adding
+        if word[0] in consonants:
+            new_word = (word + word[0])[1:]
+            print(new_word + pig_latin_adding)
+            return new_word + pig_latin_adding
+
+    else:
+        word_too_short = print("Votre mot est trop court.")
+        return word_too_short
 
 def translate(text):
     # translated_words = []
@@ -75,5 +78,6 @@ def translate(text):
     return ' '.join(map(translate_word, text.split()))
 
 
-print(translate_word("square"))
-print(translate("voici une phrase"))
+
+translate_word("tv")
+# print(translate("voici une phrase"))
