4cadc66efc6fefc6122c9d9554f09a45519f53e4
Marion Vives <mvives.dev@gmail.com>
2025-06-03 16:58:55 +0200

    refacto thanks to pylit

--- exercices/exercism_comparisons/black_jack/main.py
+++ exercices/exercism_comparisons/black_jack/main.py
@@ -1,6 +1,12 @@
+"""Functions to help play and score a game of blackjack.
+
+How to play blackjack:    https://bicyclecards.com/how-to-play/blackjack/
+"Standard" playing cards: https://en.wikipedia.org/wiki/Standard_52-card_deck
+"""
+
 numeric_cards = [list(range(2,11)), '2', '3', '4', '5', '6', '7', '8', '9', '10']
-face_cards = ["J", "Q", "K"]
-ace_card = ["A"]
+face_cards = ['J', 'Q', 'K']
+ace_card = ['A']
 cards =  numeric_cards + face_cards + ace_card
 
 def value_of_card(card):
@@ -14,15 +20,13 @@ def value_of_card(card):
     3.  '2' - '10' = numerical value.
     """
     if card in numeric_cards:
-        # print(f"{card} : It is a real card and it brings {card} points !")
         return int(card)
-    elif card == "A":
-        # print(f"{card} : It is a real card and it brings {1} point !")
+    if card == 'A':
         return 1
-    elif card in face_cards:
-        # print(f"{card} : It is a real card and it brings {10} points !")
+    if card in face_cards:
         return 10
-    raise ValueError("This is not a real card !")
+    return 'This is not a real card !'
+
 
 def higher_card(card_one, card_two):
     """Determine which card has a higher value in the hand.
@@ -30,16 +34,19 @@ def higher_card(card_one, card_two):
     :param card_one, card_two: str - cards dealt in hand.  See below for values.
     :return: str or tuple - resulting Tuple contains both cards if they are of equal value.
 
+    1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
+    2.  'A' (ace card) = 1
+    3.  '2' - '10' = numerical value.
     """
+
     if card_one in cards and card_two in cards:
         value_of_card_one = value_of_card(card_one)
         value_of_card_two = value_of_card(card_two)
-        if value_of_card_one == value_of_card_two:
-            return card_one, card_two
-        elif value_of_card_one > value_of_card_two:
-            return card_one
-        return card_two
-    raise ValueError("This is not a real card !")
+    if value_of_card_one == value_of_card_two:
+        return card_one, card_two
+    if value_of_card_one > value_of_card_two:
+        return card_one
+    return card_two
 
 def value_of_ace(card_one, card_two):
     """Calculate the most advantageous value for the ace card.
@@ -47,26 +54,34 @@ def value_of_ace(card_one, card_two):
     :param card_one, card_two: str - card dealt. See below for values.
     :return: int - either 1 or 11 value of the upcoming ace card.
 
+    1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
+    2.  'A' (ace card) = 11 (if already in hand)
+    3.  '2' - '10' = numerical value.
     """
     value_of_card_one = value_of_card(card_one)
     value_of_card_two = value_of_card(card_two)
     if card_one == 'A' or card_two == 'A':
         return 1
-    elif (value_of_card_one + value_of_card_two) <= 10:
+    if (value_of_card_one + value_of_card_two) <= 10:
         return 11
     return 1
+
 
 def is_blackjack(card_one, card_two):
     """Determine if the hand is a 'natural' or 'blackjack'.
 
     :param card_one, card_two: str - card dealt. See below for values.
     :return: bool - is the hand is a blackjack (two cards worth 21).
+
+    1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
+    2.  'A' (ace card) = 11 (if already in hand)
+    3.  '2' - '10' = numerical value.
     """
+
     face_cards.append('10')
-    # print(face_cards)
     if card_one in ace_card and card_two in face_cards:
         return True
-    elif card_two in ace_card and card_one in face_cards:
+    if card_two in ace_card and card_one in face_cards:
         return True
     return False
 
@@ -76,11 +91,13 @@ def can_split_pairs(card_one, card_two):
     :param card_one, card_two: str - cards dealt.
     :return: bool - can the hand be split into two pairs? (i.e. cards are of the same value).
     """
+
     if card_one == card_two:
         return True
-    elif card_one in face_cards and card_two in face_cards:
+    if card_one in face_cards and card_two in face_cards:
         return True
     return False
+
 
 def can_double_down(card_one, card_two):
     """Determine if a blackjack player can place a double down bet.
@@ -88,6 +105,7 @@ def can_double_down(card_one, card_two):
     :param card_one, card_two: str - first and second cards in hand.
     :return: bool - can the hand can be doubled down? (i.e. totals 9, 10 or 11 points).
     """
+
     value_of_card_one = value_of_card(card_one)
     value_of_card_two = value_of_card(card_two)
     match (value_of_card_one + value_of_card_two) :
